68cfa8a5f2785e6a5621de3af514fd72adef68d7
Haytham Chhilif <haytham.chhilif@bytedance.com>
2023-12-31 14:24:30 +0100

    chapter 13 Handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,16 +4,17 @@ import { sql } from "./db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export async function createInvoice(formData: FormData) {
-  const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(["pending", "paid"]),
-    date: z.string(),
-  });
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
 
+export async function createInvoice(formData: FormData) {
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
@@ -28,4 +29,29 @@ export async function createInvoice(formData: FormData) {
   `;
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
